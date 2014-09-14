9fa816e4c580e81dd2842a37a3f7f22da6dd9eef
Oliver Hills <oli@deathbycomputers.co.uk>
2014-09-14 15:57:06 +0800

    Ubuntu challenges 05 done

---
+++ UbuntuForumsChallenges/05-Counting_game.rb
@@ -0,0 +1,48 @@
+
+
+def randomnumber
+	1 +rand(99)
+end
+
+def tellme(thing)
+	puts thing
+end
+
+def testentry(entry, guessthis)
+	if entry > guessthis then
+		tellme("Try lower")
+		return "fail"
+	elsif entry < guessthis then
+		tellme("Try higher")
+		return "fail"
+	else
+		tellme("Got it")
+		return "win"
+	end
+end
+
+def getentry
+	entry = gets
+end
+
+def game(gamenumber)
+	result = "fail"
+	guesses = 0
+	guessthis = randomnumber
+	tellme("Try to guess a number between 1 & 100")
+	while result == "fail" do
+		entry = getentry.to_i
+		result = testentry(entry, guessthis)
+		guesses = guesses + 1
+	end
+	tellme("Well done you completed the game in #{guesses} tries you have played #{gamenumber} games")
+	return 1
+end
+
+gamenumber = 1  
+tryagain = "y"
+while tryagain == "y"
+	gamenumber = gamenumber + game(gamenumber)
+	tellme("If you want to try again please enter 'y' anything else will end the game")
+	tryagain = getentry.chomp
+end
