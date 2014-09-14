1f14de94b04700772606a6d8f89a2be8cd024bf7
Oliver Hills <oli@deathbycomputers.co.uk>
2014-09-14 11:00:56 +0800

    Ubuntu challenge 02 done

--- UbuntuForumsChallenges/02-Input_forum_details.rb
+++ UbuntuForumsChallenges/02-Input_forum_details.rb
@@ -1,44 +1,72 @@
 
-=begin
-Write a program to take a person's forum name, age, forum user id (a postive integer equal to or between 1 and 999999). The program should ask for each datum individually, and not go to the next unless the previous was valid. If there is an error, it must prompt again until it is write or the program is halted. The entry "quit" should exit the program at any question.
-
-You are LaRoza, aged 20 next year you will be 21, with user id 266234, the next user is 266235.
-=end
-
 puts "Collecting your forum details, entering quit at any prompt will exit"
 
-quit = ["quit", "Quit", "QUIT"]
 
-questions = ["name", "age", "id"]
-for question in questions do
+
+def ask(question)
 	puts "Please enter your #{question}"
-	# the .chomp is needed to remove the new line which gets adds
 	answer = gets.chomp
-		puts "You have entered your #{question} as #{answer}"
-		if question == "name" then
-			name = answer
-		elsif question == "age" then
-			if answer < 1 then
-			age = answer
-		else
-			id = answer
+	return answer
+end
+
+
+class TestAnswer 
+	def initialize(answer, question)
+		@answer = answer
+		@question = question
+	end
+	def put
+		puts @answer
+		puts @question
+	end
+	def checkquit
+		quit = ["quit", "Quit", "QUIT"]
+		if quit.include?(@answer) then
+			exit
 		end
+	end
+	def checkint
+		answerint = @answer.to_i
+		if answerint.to_i < 1 then
+			sane = "false"
+		elsif answerint > 999999 then
+			sane = "false"
+		else
+			sane = "true"
+		end
+		return sane
 	end
 end
 
 
 
-def checksanity(question)
-	if quit.include?(answer) then
-		break
-	elsif question == "name"
-		sane = "true"
-	else
-		
+questions = ["name", "age", "id"]
+for question in questions do
+sane = "false"
+	while sane == "false" do
+		answer = ask(question)
+	
+		test = TestAnswer.new(answer, question)
 
+		test.checkquit
 
+		if question == "name" then
+			sane = "true"
+			name = answer
+		elsif question == "age" then
+			sane = test.checkint
+			if sane == "true" then
+				age = answer
+			end
+		else 
+			sane = test.checkint
+			if sane == "true" then
+				id = answer
+			end
+		end
+	end
 
-
+end
 
 
 
